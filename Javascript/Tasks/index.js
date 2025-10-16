ececa051040b0df2ca35c5b285504efeabba9c49
Asif Ahmed Howlader <ahmedasifhowlader@gmail.com>
2025-10-16 17:04:10 +0200

    added challange pack codes

---
+++ Javascript/Tasks/index.js
@@ -0,0 +1,12 @@
+let weather = "Sunny";
+let temperature = 86;
+
+if (weather == "Sunny") {
+  if (temperature > 80) {
+    console.log("Yay, let's go to the beach! 🏖️");
+  } else {
+    console.log("Let's do something fun outdoors! ⛰️");
+  }
+} else {
+  console.log("Let's watch some shows on the couch! 🍿");
+}
---
+++ Javascript/Tasks/night-owl.js
@@ -0,0 +1,7 @@
+let wakeUp = 7;
+
+if (wakeUp < 8) {
+  console.log("Early bird gets the worm! 🌅");
+} else {
+  console.log("But who wants the worm? 🌃");
+}
