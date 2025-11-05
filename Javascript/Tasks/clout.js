5a2dd4c446b6ae5bfb0ace8190199fef20f302a7
Asif Ahmed Howlader <ahmedasifhowlader@gmail.com>
2025-11-05 17:33:04 +0100

    sum and average from an array

--- Javascript/Tasks/clout.js
+++ Javascript/Tasks/clout.js
@@ -3,5 +3,13 @@ const recentInstagramViews = [936, 2576, 453, 7013, 5489, 7402, 3921];
 const recentYouTubeViews = [2300, 453, 5222, 989, 6733, 7402, 2789];
 
 function mean(viewsArray) {
-  let totalViews = "";
+  const totalViews = viewsArray.reduce((total, amount) => total + amount);
+
+  const average = totalViews / recentTikTokViews.length;
+  return average;
+}
+
+function median(viewsArray) {
+  let sortedStats = "";
+  let middleIndex = "";
 }
