d996fea6e519a7daf6df358f51c5a014dcf55862
Asif Ahmed Howlader <ahmedasifhowlader@gmail.com>
2025-11-06 13:31:25 +0100

    median and mean from an array

--- Javascript/Tasks/clout.js
+++ Javascript/Tasks/clout.js
@@ -5,11 +5,32 @@ const recentYouTubeViews = [2300, 453, 5222, 989, 6733, 7402, 2789];
 function mean(viewsArray) {
   const totalViews = viewsArray.reduce((total, amount) => total + amount);
 
-  const average = totalViews / recentTikTokViews.length;
+  const average = totalViews / viewsArray.length;
   return average;
 }
 
 function median(viewsArray) {
-  let sortedStats = "";
-  let middleIndex = "";
+  let sortedStats = viewsArray.sort(function (a, b) {
+    return a - b;
+  });
+  // console.log(sortedStats);
+  const length = viewsArray.length;
+  const middleIndex = Math.floor(length / 2);
+  if (length / 2 == 0) {
+    return (viewsArray[middleIndex - 1] + viewsArray[middleIndex]) / 2;
+  } else {
+    return viewsArray[middleIndex];
+  }
 }
+//Tiktok
+console.log("Tiktok");
+console.log(`Mean: ${mean(recentTikTokViews)}`);
+console.log(`Median: ${median(recentTikTokViews)}`);
+//Instagram
+console.log("Instagram");
+console.log(`Mean: ${mean(recentInstagramViews)}`);
+console.log(`Median: ${median(recentInstagramViews)}`);
+//Youtube
+console.log("Youtube");
+console.log(`Mean: ${mean(recentYouTubeViews)}`);
+console.log(`Median: ${median(recentYouTubeViews)}`);
