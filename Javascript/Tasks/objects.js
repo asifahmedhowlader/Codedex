12453634be397ac36b4fade8eb34a0718e1ac1c4
Asif Ahmed Howlader <ahmedasifhowlader@gmail.com>
2025-11-07 14:07:26 +0100

    objects

---
+++ Javascript/Tasks/objects.js
@@ -0,0 +1,15 @@
+let car = {
+  model: "Ford Focus",
+  year: 2025,
+  color: "Green",
+  used: false,
+};
+if (car.used == true) {
+  console.log(
+    `I'm looking for a ${car.color} ${car.year} ${car.model} that is used.`
+  );
+} else {
+  console.log(
+    `I'm looking for a ${car.color} ${car.year} ${car.model} that is new.`
+  );
+}
