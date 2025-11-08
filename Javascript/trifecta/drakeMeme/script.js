cac9b2e9d097dc348389034fb4807a47b3f2317e
Asif Ahmed Howlader <ahmedasifhowlader@gmail.com>
2025-11-08 23:47:31 +0100

    JS DOM

---
+++ Javascript/trifecta/drakeMeme/script.js
@@ -0,0 +1,5 @@
+let drakePicTwo = document.getElementById("drake-pic-2");
+drakePicTwo.src =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDc3yORMvLZYl7T6scyVoGYQctvh8Kyz7JJA&s";
+let heading = document.getElementById("heading-2");
+heading.innerHTML = "Learn By Doing";
